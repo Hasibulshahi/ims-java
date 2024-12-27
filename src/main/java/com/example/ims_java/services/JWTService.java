45919863fe3264fbb021703af3c54d9816e5ad87
Hasibul Hassan <shahi_aust@hotmail.com>
2024-12-27 20:13:18 +0600

    update

--- src/main/java/com/example/ims_java/services/JWTService.java
+++ src/main/java/com/example/ims_java/services/JWTService.java
@@ -1,7 +1,6 @@
 package com.example.ims_java.services;
 
 import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 import io.jsonwebtoken.Jwts;
@@ -9,7 +8,6 @@ import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
-import java.security.Key;
 import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
 import java.util.Date;
@@ -46,16 +44,6 @@ public class JWTService {
                 .compact();
 
     }
-//    public String generateToken(String username) {
-//        Map<String, Object> claims = new HashMap<>();
-//        return Jwts.builder()
-//                .setClaims(claims) // Set claims
-//                .setSubject(username) // Set subject
-//                .setIssuedAt(new Date(System.currentTimeMillis())) // Set issued date
-//                .setExpiration(new Date(System.currentTimeMillis() + 60 * 60 * 30 * 1000)) // Set expiration (30 hours)
-//                .signWith(getKey(), SignatureAlgorithm.HS256) // Sign the token with a key and algorithm
-//                .compact(); // Build the token
-//    }
 
     private SecretKey getKey() {
         byte[] keyBytes = Decoders.BASE64.decode(secretkey);
@@ -80,14 +68,6 @@ public class JWTService {
                 .parseSignedClaims(token)
                 .getPayload();
     }
-
-//    private Claims extractAllClaims(String token) {
-//        return Jwts.parserBuilder()
-//                .setSigningKey(getKey()) // Set the signing key for verification
-//                .build() // Build the parser
-//                .parseClaimsJws(token) // Parse the signed JWT and extract the claims
-//                .getBody(); // Get the claims body
-//    }
 
     public boolean validateToken(String token, UserDetails userDetails) {
         final String userName = extractUserName(token);
